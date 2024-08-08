34112dbf60231e38d8c3de123dbe570c5aa9016c
SamuelMagalhaes <samuel.signas@gmail.com>
2024-08-08 20:53:54 -0300

    primeiro commit

---
+++ src/Routes.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login"
+import Success from "./pages/Success";
+import Profile from "./pages/Profile";
+import Products from "./pages/Products"
+import Cart from "./pages/Cart"
+import Confirm from "./pages/Confirm"
+import Orders from "./pages/Orders"
+import ProductDetail from "./pages/ProductDetail"
+
+function AppRoutes(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Register" element={<Register />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Products" element={<Products />}></Route>
+        <Route path="/Cart" element={<Cart />}></Route>
+        <Route path="/Confirm" element={<Confirm />}></Route>
+        <Route path="/Orders" element={<Orders />}></Route>
+        <Route path="/ProductDetail" element={<ProductDetail />}></Route>
+        <Route path="/Profile" element={<Profile />}></Route>
+        <Route path="/Success" element={<Success />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+export default AppRoutes
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header"
+import Footer from "../../components/Footer/Footer"
+
+function Home(){
+  return (
+    <main>
+      <Header/>
+      <Footer/>
+    </main>
+  )
+}
+
+export default Home
