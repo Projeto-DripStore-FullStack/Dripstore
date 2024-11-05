b83728856605387f66f0b6b3863164cff7a85643
Giovanni Azevedo <giovanniadev@gmail.com>
2024-11-05 16:57:47 -0300

    criação de usuário pela pagina criar conta e redirecionamento para login

--- src/components/FormCrieSuaConta/FormCrieSuaConta.jsx
+++ src/components/FormCrieSuaConta/FormCrieSuaConta.jsx
@@ -1,15 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import RedesSociaisDeAcesso from "../../assets/RedesSociaisDeAcesso.png";
 import { InputParaForm } from "../InputParaForm/InputParaForm";
 import "./FormCrieSuaConta.css";
 
 export const FormCrieSuaConta = () => {
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleAcessarConta = () => {
+    navigate(`/CreateAccount`);
+  };
+
   return (
     <>
       <div className="formAcessarContaBox">
         <h3 className="titleFormformAcessarConta">Crie Sua Conta</h3>
         <p className="pFormformAcessarConta">
           Já possui uma conta? Então entre{" "}
-          <a className="aForm3" href="/CreateAccount">
+          <a className="aForm3" href="/Login">
             aqui.
           </a>
         </p>
@@ -19,7 +30,12 @@ export const FormCrieSuaConta = () => {
             placeholder="Insira seu login ou email"
           />
         </div>
-        <button className="btnFormformAcessarConta">Acessar Conta</button>
+        <button
+          className="btnFormformAcessarConta"
+          onClick={handleAcessarConta}
+        >
+          Acessar Conta
+        </button>
         <div className="divBaseFormformAcessarConta">
           <p className="pFormformAcessarConta">Ou faça Login Com</p>
           <img
