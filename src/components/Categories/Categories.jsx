ec6029075791e99ad508af6601dafd98b8d2c351
SamuelMagalhaes <samuel.signas@gmail.com>
2024-11-27 09:37:32 -0300

    estilizando os checkboxes

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -16,9 +16,9 @@ export const Categories = () => {
   const imageMapping = {
     Camisetas: TShirt,
     Calças: Pants,
-    Tênis: Sneakers,
+    Tenis: Sneakers,
     Bonés: Bone,
-    Headphones: Headphones
+    Fones: Headphones
   };
 
   const getCategoriaPecas = async () => {
